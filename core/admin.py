49fae66c3d3a305187ba6d224dbe61b427b7be61
Augusto <augusto.bendlin1997@gmail.com>
2022-02-11 00:50:29 -0300

    Primeira versão

---
+++ core/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from core.models import Categoria, Compra, Fabricante, ItensCompra, Pneu
+
+admin.site.register(Categoria)
+admin.site.register(Fabricante)
+admin.site.register(Pneu)
+
+class ItensInline(admin.TabularInline):
+  model = ItensCompra
+
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+  inlines = (ItensInline,)
---
+++ core/models.py
@@ -0,0 +1,42 @@
+from random import choices
+from django.db import models
+from django.contrib.auth.models import User
+
+class Categoria(models.Model):
+  modelo = models.CharField(max_length=255)
+
+  def __str__(self):
+      return self.modelo
+
+class Fabricante(models.Model):
+  nome = models.CharField(max_length=255)
+  endereco = models.CharField(max_length=255)
+
+  def __str__(self):
+      return self.nome
+
+class Pneu(models.Model):
+  codigoFabricacao = models.CharField(max_length=255)
+  medida = models.CharField(max_length=255, default="")
+  preco = models.FloatField()
+  categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="pneus")
+  fabricante = models.ForeignKey(Fabricante, on_delete=models.PROTECT, related_name="pneus")
+
+  def __str__(self):
+      return "%s (%s)" %(self.fabricante, self.categoria)
+
+class Compra(models.Model):
+
+  class StatusCompra(models.IntegerChoices):
+    CARRINHO = 1, 'Carrinho'
+    REALIZADO = 2, 'Realizado'
+    PAGO = 3, 'Pago'
+    ENTREGUE = 4, 'Entregue'
+
+  usuario = models.ForeignKey(User, on_delete=models.PROTECT, related_name="compras")
+  status = models.IntegerField(choices=StatusCompra.choices, default=StatusCompra.CARRINHO)
+
+class ItensCompra(models.Model):
+  compra = models.ForeignKey(Compra, on_delete=models.CASCADE, related_name="itens")
+  pneu = models.ForeignKey(Pneu, on_delete=models.PROTECT, related_name="+")
+  quantidade = models.IntegerField()
